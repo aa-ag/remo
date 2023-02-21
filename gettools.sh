66f4676ffcd7e683bc18f8bd3dbfa4b1a0d6f0ac
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-21 17:44:41 -0600

    Add remote setup once VM is setup

---
+++ gettools.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install --assume-yes wget tasksel
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
