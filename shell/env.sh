155d13406ef108df5d3071055d90dc751abb942a
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-22 11:49:13 -0600

    Add command to check that Remote is working as expected

--- shell/env.sh
+++ shell/env.sh
@@ -1,3 +1,4 @@
 sudo tasksel install ubuntu-desktop
 sudo bash -c ‘echo “exec /etc/X11/Xsession /usr/bin/gnome-session” > /etc/chrome-remote-desktop-session’
 sudo reboot
+sudo systemctl status chrome-remote-desktop@$USER
