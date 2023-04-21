f6d655690396817f9c3ecc92b21850aad96c4875
aa-ag <aaron.aguerrevere@gmail.com>
2023-04-21 09:22:59 -0500

    Reword line 5 to reflect client install

--- shell/ubuntu/u.sh
+++ shell/ubuntu/u.sh
@@ -2,3 +2,4 @@ sudo apt update
 sudo apt install ubuntu-desktop
 sudo apt install tightvncserver
 sudo apt install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+# install vnc client
